dca800af734e4bd0cc3d13342abc29fbdfcc728a
Shourya Shikhar <danger.ahead@pm.me>
2023-01-27 17:17:10 +0530

    added bootstrap

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,41 @@
-import { Outlet } from "react-router-dom";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Link, Outlet } from "react-router-dom";
+import { indexRoute } from "../../Routes";
 import "./NavBar.style.css";
 
 const NavBar = () => {
   return (
-    <div className="d-flex app">
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>
+              <Link to={indexRoute}>Home</Link>
+            </Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Something</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Something
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <Outlet />
-    </div>
+    </>
   );
 };
 
