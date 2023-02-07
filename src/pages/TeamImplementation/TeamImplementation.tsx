ab4a130d8c6c4e0e346e90ff585c07696ae306e3
mayukh551 <may14ukh@gmail.com>
2023-02-07 18:43:13 +0530

    Implementation team ready

--- src/pages/TeamImplementation/TeamImplementation.tsx
+++ src/pages/TeamImplementation/TeamImplementation.tsx
@@ -1,25 +1,50 @@
-const ProjectImplementation = () => {
+import impl_team_data from "./data";
+import '../Teams/Teams.style.css';
+
+const TeamImplementation = () => {
   return (
-    <main className="w-100">
-      <div className={"w-100 position-relative"}>
-        <img
-          className={"d-block"}
-          src={"https://sih.gov.in/img/project-implementation.jpg"}
-          alt={"projectImplementation"}
-          style={{ height: "100%", width: "100%", objectFit: "cover" }}
-        />
-        <h1
-          className="carousel-caption fw-bold text-center "
-          style={{
-            fontSize:
-              "min(max(10px, calc(0.625rem + ((1vw - 0.01px) * 2.6055))), 60px)",
-          }}
-        >
-          TEAM IMPLEMENTATION
-        </h1>
+    <div>
+      <main className="w-100">
+        <div className={"w-100 position-relative"}>
+          <img
+            className={"d-block"}
+            src={"https://sih.gov.in/img/project-implementation.jpg"}
+            alt={"projectImplementation"}
+            style={{ height: "100%", width: "100%", objectFit: "cover" }}
+          />
+          <h1
+            className="carousel-caption fw-bold text-center "
+            style={{
+              fontSize:
+                "min(max(10px, calc(0.625rem + ((1vw - 0.01px) * 2.6055))), 60px)",
+            }}
+          >
+            IMPLEMENTATION TEAM
+          </h1>
+        </div>
+      </main>
+      <div className="container pt-5 pb-5 team-wrapper">
+        {impl_team_data.map((member, index) => {
+          return (
+            <div key={index} className="member-container">
+              {member.img && (
+                <img
+                  className="member-img mb-3"
+                  src={member.img}
+                  alt={`${member.name}`}
+                />
+              )}
+              <h6 className="member-name">{member.name}</h6>
+              <h6 className="member-designation mb-3">{member.position}</h6>
+              <h6 className="text-center member-committee">
+                {member.designation}
+              </h6>
+            </div>
+          );
+        })}
       </div>
-    </main>
+    </div>
   );
 };
 
-export default ProjectImplementation;
+export default TeamImplementation;
---
+++ src/pages/TeamImplementation/data.ts
@@ -0,0 +1,69 @@
+import abhijtSir from '../../assets/implementation-team/Prof. Abhijit.png';
+import pranashSir from '../../assets/implementation-team/Mr. Pranabesh Das.png';
+import anirbanSir from '../../assets/implementation-team/Prof.Anirban Mukherjee.png';
+import aloknathSir from '../../assets/implementation-team/Dr. Aloknath De.png';
+import amlanSir from '../../assets/implementation-team/Prof..Amlan Chakrabrti.png';
+import joydeepSir from '../../assets/implementation-team/Joydeep Banerjee.png';
+import swagatamSir from '../../assets/implementation-team/Prof.Swagatam Das.png';
+
+type impl_team_type = {
+    img: string,
+    name: string,
+    position: string,
+    designation: string
+}
+
+
+const impl_team_data: impl_team_type[] = [
+    {
+        img: abhijtSir,
+        name: 'Prof. Abhijit Chakraborty',
+        position: 'Chief Patron',
+        designation: 'Chairman, BOM, RCCIIT & Ex.VC of Jadavpur University & Professor of IIEST'
+    },
+    {
+        img: pranashSir,
+        name: 'Mr. Pranabesh Das',
+        position: 'Patron',
+        designation: 'Chairman, BOG, RCCIIT, Ex.Director of Technical Education, Govt.of West Bengal'
+    },
+    {
+        img: anirbanSir,
+        name: 'Prof. Anirban Mukherjee',
+        position: 'Patron',
+        designation: 'Principal(ofg.), RCCIIT'
+    },
+    {
+        img: aloknathSir,
+        name: 'Dr. Alokenath De',
+        position: 'Chief Mentor',
+        designation: 'Ex. Corporate Vice President of Samsung Electronics and Chief Technology Officer of Samsung R&D Institute India, Bangalore, Chair, IEEE Bangalore section'
+    },
+    {
+        img: 'https://rcciit.org/images/staff/ee/f/alok.jpg',
+        name: 'Dr. Alok Kole',
+        position: 'Converner',
+        designation: 'Professor, Dept.of EE & President of Institution’s Innovation Council, RCCIIT'
+    },
+    {
+        img: amlanSir,
+        name: 'Prof. Amlan Chakrabrti',
+        position: 'Adviser',
+        designation: 'Professor and Director, A.K. Choudhury School of Information Technology University of Calcutta'
+    },
+    {
+        img: joydeepSir,
+        name: 'Joydeep Banerjee',
+        position: 'Adviser',
+        designation: 'Global CoC, Mainframe Modernization Service, IBM Consulting'
+    },
+    {
+        img: swagatamSir,
+        name: 'Prof. Swagatam Das',
+        position: 'Adviser',
+        designation: 'Associate Professor and Head Electronics and Communication Sciences Unit, Indian Statistical Institute'
+    }
+]
+
+
+export default impl_team_data;
