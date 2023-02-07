c27352841dbe1a07d4e5f2415c47780f07001da2
mayukh551 <may14ukh@gmail.com>
2023-02-07 16:16:55 +0530

    team updates done

--- src/pages/Teams/TeamData.ts
+++ src/pages/Teams/TeamData.ts
@@ -1,99 +1,98 @@
 import img1 from "../../assets/sandip saha.jpg";
-import profileIcon from "../../assets/profile-circle-svgrepo-com.svg";
 import { teamMemberType } from "../../components/Models/teamDataType";
 
 export const sbhTeam: teamMemberType[] = [
   {
-    img: profileIcon,
-    name: "Dr Aloknath De",
-    designation: "Chief Mentor, SBH 2023",
+    img: "https://www.indicon2022.org/images/Advisory%20Committee%20Photos/Dr.%20Aloknath%20De.png",
+    name: "Dr. Aloknath De",
+    designation: "Chief Mentor, SBH2023",
     committee:
-      "Ex.corporate vice president of Samsung Electronics and chief technology officer of Samsung R&D Institute India, Bangalore, Chair, IEEE Bangalore section",
+      "Ex. Corporate Vice President of Samsung Electronics and Chief Technology Officer of Samsung R&D Institute India, Bangalore, Chair, IEEE Bangalore section",
   },
   {
     img: "https://rcciit.org/images/staff/ee/f/alok.jpg",
-    name: "Dr.Alok Kole",
-    designation: "Convenor, SBH'23",
+    name: "Dr. Alok Kole",
+    designation: "Convener, SBH2023",
     committee: "Coordinator, Promotion and Sponsorship Sub-Committee",
   },
   {
     img: "https://rcciit.org/images/staff/it/f/Hrishikesh.jpg",
     name: "Dr. Hrishikesh Bhaumik",
-    designation: "Jt. Coordinator, SBH'23",
+    designation: "Jt. Coordinator, SBH2023",
     committee: "Promotion and Sponsorship Sub-Committee",
   },
   {
     img: "https://rcciit.org/images/staff/it/f/indrajit.jpg",
     name: "Dr. Indrajit Pan",
-    designation: "Coordinator, SBH'23",
+    designation: "Coordinator, SBH2023",
     committee: "Technical Collaborations Sub-Committee",
   },
   {
     img: "https://rcciit.org/images/staff/it/f/Hiranmoy.jpg",
     name: "Dr. Hiranmoy Roy",
-    designation: "Convenor, SBH'23",
+    designation: "Jt. Convener, SBH2023",
     committee: "Coordinator, Program Design and Technical Sub-Committee",
   },
   {
     img: "https://rcciit.org/images/staff/ece/f/arpan.jpg",
     name: "Dr. Arpan Deyasi",
-    designation: "Jt. Coordinator, SBH'23",
+    designation: "Jt. Coordinator, SBH2023",
     committee: "Program Design and Technical Sub-Committee",
   },
   {
     img: "https://rcciit.org/images/staff/it/f/Soumen.jpg",
     name: "Mr. Soumen  Mukherjee",
-    designation: "Coordinator, SBH'23",
+    designation: "Coordinator, SBH2023",
     committee:
       "Infrastructure, Logistics & Hospitality Management Sub-Committee",
   },
   {
     img: img1,
     name: "Mr. Sandip Saha",
-    designation: "Jt. Coordinator, SBH'23",
+    designation: "Jt. Coordinator, SBH2023",
     committee:
       "Infrastructure, Logistics & Hospitality Management Sub-Committee",
   },
   {
     img: "https://rcciit.org/images/staff/ee/f/Debasish.jpg",
     name: "Dr. Debasish Mondal",
-    designation: "Coordinator, SBH'23",
+    designation: "Coordinator, SBH2023",
     committee: "Finance Sub-Committee",
   },
   {
     img: "https://rcciit.org/images/staff/ece/f/arpitaghosh.jpg",
     name: "Dr. Arpita Ghosh",
-    designation: "Coordinator, SBH'23",
+    designation: "Coordinator, SBH2023",
     committee: "Event Management Sub-Committee",
   },
   {
     img: "https://rcciit.org/images/staff/ee/f/Sarbojit.jpg",
     name: "Mr. Sarbojit Mukherjee",
-    designation: "Jt. Coordinator, SBH'23",
+    designation: "Jt. Coordinator, SBH2023",
     committee: "Event Management Sub-Committee",
   },
   {
     img: "https://rcciit.org/images/staff/ece/f/Abhishek.jpg",
     name: "Dr. Abhishek Basu",
-    designation: "Coordinator, SBH'23",
+    designation: "Coordinator, SBH2023",
     committee: "Invitation & Liaison Sub-Committee",
   },
   {
     img: "https://rcciit.org/images/staff/aeie/f/srijan.jpg",
     name: "Dr. Srijan Bhattacharya",
-    designation: "Jt. Coordinator, SBH'23",
+    designation: "Jt. Coordinator, SBH2023",
     committee: "Invitation & Liaison Sub-Committee",
   },
   {
     img: "https://rcciit.org/images/staff/ece/f/Soham.jpg",
     name: "Dr. Soham Sarkar",
-    designation: "Jt. Coordinator, SBH'23",
+    designation: "Jt. Coordinator, SBH2023",
     committee: "Publication & Media Sub-Committee",
   },
   {
     img: "https://rcciit.org/images/staff/cse/f/Anup.jpg",
     name: "Dr. Anup Kumar Kolya",
-    designation: "Jt. Coordinator, SBH'23",
+    designation: "Jt. Coordinator, SBH2023",
     committee: "Finance Sub-Committee",
   },
 ];
