aa54d18ab8315575aab14d07001b10a98af40a77
Shourya Shikhar <danger.ahead@pm.me>
2023-03-09 02:01:50 +0530

    added comments in circle.tsx

--- src/components/Blob/Circle.tsx
+++ src/components/Blob/Circle.tsx
@@ -1,3 +1,18 @@
+/**
+ * @description: the blob circle component is a colored blob circle with a blur effect and positioned absolute
+ * @author: @danger-ahead
+ * @param color: the color of the circle
+ * @param width: the width of the circle
+ * @param height: the height of the circle
+ * @param blur: the blur radius of the circle
+ * @param top: the top position of the circle
+ * @param left: the left position of the circle
+ * @param right: the right position of the circle
+ * @param bottom: the bottom position of the circle
+ * @param zIndex: the z-index of the circle
+ * @returns a blob circle
+ */
+
 const Circle = ({
   color = "#F88208",
   width = "200px",
