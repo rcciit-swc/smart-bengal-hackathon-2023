8344cadf17fb71a84e55ef9c5a34c74a7d492c43
Sansrita <sansritasaha@gmail.com>
2024-01-10 00:52:24 +0530

    executive team page update

--- src/pages/Teams/TeamData.ts
+++ src/pages/Teams/TeamData.ts
@@ -2,24 +2,12 @@ import img1 from "../../assets/sandip saha.jpg";
 import { teamMemberType } from "../../components/Models/teamDataType";
 
 export const sbhTeam: teamMemberType[] = [
-  {
-    img: "https://i.imgur.com/weq8eKV.png",
-    name: "Dr. Aloknath De",
-    designation: "Chief Mentor, SBH2023",
-    committee:
-      "Ex. Corporate Vice President of Samsung Electronics and Chief Technology Officer of Samsung R&D Institute India, Bangalore, Chair, IEEE Bangalore section",
-  },
+  
   {
     img: "https://rcciit.org/images/staff/ee/f/alok.jpg",
     name: "Dr. Alok Kole",
     designation: "Convener, SBH2023",
     committee: "Coordinator, Promotion and Sponsorship Sub-Committee",
-  },
-  {
-    img: "https://rcciit.org/images/staff/it/f/Hiranmoy.jpg",
-    name: "Dr. Hiranmoy Roy",
-    designation: "Jt. Convener, SBH2023",
-    committee: "Coordinator, Program Design and Technical Sub-Committee",
   },
   {
     img: "https://rcciit.org/images/staff/it/f/Hrishikesh.jpg",
@@ -28,42 +16,10 @@ export const sbhTeam: teamMemberType[] = [
     committee: "Promotion and Sponsorship Sub-Committee",
   },
   {
-    img: "https://rcciit.org/images/staff/it/f/indrajit.jpg",
-    name: "Dr. Indrajit Pan",
-    designation: "Coordinator, SBH2023",
-    committee: "Technical Collaborations Sub-Committee",
-  },
-  {
     img: "https://rcciit.org/images/staff/ece/f/arpan.jpg",
     name: "Dr. Arpan Deyasi",
     designation: "Jt. Coordinator, SBH2023",
     committee: "Program Design and Technical Sub-Committee",
-  },
-  {
-    img: "https://rcciit.org/images/staff/it/f/Soumen.jpg",
-    name: "Mr. Soumen  Mukherjee",
-    designation: "Coordinator, SBH2023",
-    committee:
-      "Infrastructure, Logistics & Hospitality Management Sub-Committee",
-  },
-  {
-    img: img1,
-    name: "Mr. Sandip Saha",
-    designation: "Jt. Coordinator, SBH2023",
-    committee:
-      "Infrastructure, Logistics & Hospitality Management Sub-Committee",
-  },
-  {
-    img: "https://rcciit.org/images/staff/ee/f/Debasish.jpg",
-    name: "Dr. Debasish Mondal",
-    designation: "Coordinator, SBH2023",
-    committee: "Finance Sub-Committee",
-  },
-  {
-    img: "https://rcciit.org/images/staff/ece/f/arpitaghosh.jpg",
-    name: "Dr. Arpita Ghosh",
-    designation: "Coordinator, SBH2023",
-    committee: "Event Management Sub-Committee",
   },
   {
     img: "https://rcciit.org/images/staff/ee/f/Sarbojit.jpg",
@@ -75,12 +31,6 @@ export const sbhTeam: teamMemberType[] = [
     img: "https://rcciit.org/images/staff/ece/f/Abhishek.jpg",
     name: "Dr. Abhishek Basu",
     designation: "Coordinator, SBH2023",
-    committee: "Invitation & Liaison Sub-Committee",
-  },
-  {
-    img: "https://rcciit.org/images/staff/aeie/f/srijan.jpg",
-    name: "Dr. Srijan Bhattacharya",
-    designation: "Jt. Coordinator, SBH2023",
     committee: "Invitation & Liaison Sub-Committee",
   },
   {
