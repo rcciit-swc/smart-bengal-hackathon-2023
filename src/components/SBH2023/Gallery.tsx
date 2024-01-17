6eb71edb88213a69a524c71fb599da383ff05461
Esha Tokedar <eshatokedar@gmail.com>
2024-01-17 18:38:20 +0530

    Gallery added to sbh 2023

--- src/components/SBH2023/Gallery.tsx
+++ src/components/SBH2023/Gallery.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import GalleryCard from './GalleryCard'
+import { FadeIn } from "react-slide-fade-in/dist/fade-in";
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
+    <>
+     <div className="pt-5 w-100 h-90 d-flex flex-column align-items-center text-center position-relative">
+      <h5
+        style={{ color: `var(--heading-color)` }}
+        className="caveat text-uppercase fw-bold fs-2"
+      >
+        Smart India Hackathon 2023
+      </h5>
+      <h1
+        style={{ color: "var(--primary-color)" }}
+        className="text-uppercase fw-bold caveat"
+      >
+        Gallery
+      </h1>
+      <FadeIn
+        from="bottom"
+        positionOffset={200}
+        triggerOffset={0}
+        delayInMilliseconds={100}
+      >
+      </FadeIn>
+      <p>Memories or some bullshit</p>
+      </div>
+    <GalleryCard/>
+    </>
   )
 }
 
--- src/components/SBH2023/GalleryCard.tsx
+++ src/components/SBH2023/GalleryCard.tsx
@@ -1,9 +1,68 @@
 import React from 'react'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const gallery = [
+  {
+    image:
+      "https://i.pinimg.com/736x/ff/6a/89/ff6a89d1454672a134e5a60b6157f75e.jpg",
+  },
+  {
+    image:
+      "https://i.pinimg.com/1200x/9c/a1/cf/9ca1cf7df797d13fda3d4eb052ba2d25.jpg",
+  },
+  {
+    image:
+      "https://i.pinimg.com/originals/77/45/70/7745709c4859a8664ff4d6cc7b871fe5.jpg",
+  },
+  {
+    image:
+      "https://i.pinimg.com/originals/82/36/24/8236242a5d6f9f900cea70da3762008b.jpg",
+  },
+];
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 3,
+  },
+  desktop: {
+    breakpoint: { max: 2000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 const GalleryCard = () => {
-  return (
-    <div>GalleryCard</div>
+  return (  
+         <div className="flex flex-row  justify-center items-center gap-5">
+        <Carousel
+          responsive={responsive}
+          className="w-[400px] 2xl:w-[1300px] h-[500px] flex flex-row items-center px-20"
+          arrows={true}
+          showDots={true}
+          
+          transitionDuration={100}
+          infinite={true}
+          autoPlay={true}
+          autoPlaySpeed={3000}
+        >
+         {gallery.map((card, index) => (
+            <div key={index}>
+              <img src={card.image} alt={`Gallery ${index}`} 
+               style={{ maxWidth: '90%', maxHeight: '90%', borderRadius:'2rem',paddingLeft:'15%',paddingRight:'15%',paddingBottom:'5%'}}/>
+            </div>
+          ))}
+        </Carousel>
+      </div>
   )
-}
+};
 
 export default GalleryCard
