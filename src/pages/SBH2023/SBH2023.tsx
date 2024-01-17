e75f7e92f4499d588c01bf7c13211cb68778fb55
soumyarajbag <soumyarajbag@gmail.com>
2024-01-17 21:19:17 +0530

    add:sbh-2023 elements

--- src/pages/SBH2023/SBH2023.tsx
+++ src/pages/SBH2023/SBH2023.tsx
@@ -1,10 +1,19 @@
+import Committe from '../../components/SBH2023/Committe'
+import Finale from '../../components/SBH2023/Finale'
 import Gallery from '../../components/SBH2023/Gallery'
+import Impact from '../../components/SBH2023/Impact'
 import Supporters from '../../components/SBH2023/Supporters'
+import Winners from '../../components/SBH2023/Winners'
 import Themes from '../Home/component.body.themes'
 
 const SBH2023 = () => {
   return (
     <div>
+      <Impact />
+      <Finale />
+      <Finale />
+      <Winners />
+      <Committe />
         <Gallery />
         <Themes/>
         <Supporters />
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx,html,css}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
