947ab78882ea03363a245649995e69fe86167bac
Debopom-Banerjee <debopombanerjee752@gmail.com>
2024-01-27 01:32:04 +0530

    something done

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -61,7 +61,7 @@ const CarouselContainer = () => {
               className="d-block w-100"
               height="500px"
               width="auto"
-              src="https://i.imgur.com/iG88HEb.jpg"
+              src="https://imgur.com/cBNWEIV.jpg"
               alt="Second slide"
             />
         </Carousel.Item>
