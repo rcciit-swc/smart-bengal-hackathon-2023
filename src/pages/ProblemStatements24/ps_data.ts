a7ce4624fba7799d93ee5ed133cb7e941e1f8b99
PratyushPal29 <29palpratyush04@gmail.com>
2024-02-06 16:27:11 +0530

    new file:   src/pages/ProblemStatements24/ps_data.ts 	modified:   src/pages/ProblemStatements24/ProblemStatements24.tsx 	modified:   src/pages/ProblemStatements24/ps_data.ts

---
+++ src/pages/ProblemStatements24/ps_data.ts
@@ -0,0 +1,73 @@
+export const psData = [
+    {
+      psno: "SBHRCCIIT001",
+      category: "Software",
+      theme: "Image Processing",
+      problemStat: "Detection of person talking with mobile in no mobile zone",
+      desc: "It is a regular practice that people use mobile phones while walking in roads, roaming around near the railway tracks, no mobile phone zones etc. A complete real-time solution with alert and fine system is to be developed. (Solution can use both hardware and software)."
+    },
+    {
+      psno: "SBHRCCIIT002",
+      category: "Software",
+      theme: "Smart Automation",
+      problemStat: "A scanner app to generate csv data file",
+      desc: "Develop a scanner app which will scan the document and create a table data in csv file. It is very useful for exam cell. Exam cell can easily generate student semester wise marks table in csv file. If the scanner continue scanning same types of results for multiple student then same csv file will be modified."
+    },
+    {
+      psno: "SBHRCCIIT003",
+      category: "Hardware",
+      theme: "Clean/Green Technology",
+      problemStat: "Water quality prediction and categorization",
+      desc: "A hybrid system to predict the water quality and categorize it based on its use like drinkable, useful for fish in the aquarium, useful for washing cloths, etc."
+    },
+    {
+      psno: "SBHRCCIIT004",
+      category: "Hardware",
+      theme: "Clean/Green Technology",
+      problemStat: "Plastic waste detection and separation",
+      desc: "A complete hybrid system to detect the plastic objects and separate it from other waste."
+    },
+    {
+      psno: "SBHRCCIIT005",
+      category: "Software",
+      theme: "Miscellaneous",
+      problemStat: "Gaming monitoring app",
+      desc: "An android app which will monitor the other mobile gaming apps. This app will take user input in terms of times and days to access the existing gaming apps. If the gaming app(s) is used more than the daily threshold limits (time taken by user), the developed app will stop the gaming app. If the gaming app(s) is used more than the threshold days (taken input by user) in a week, the developed app will block the gaming app (s)."
+    },
+    {
+      psno: "SBHRCCIIT006",
+      category: "Software",
+      theme: "Smart Education",
+      problemStat: "Automatic optimize class routine generation",
+      desc: "This application will generate an optimize class routine for the given input constrains, i.e., faculty timing, class timing, number of periods, tiffin time etc."
+    },
+    {
+      psno: "SBHRCCIIT007",
+      category: "Software",
+      theme: "Smart Education",
+      problemStat: "Automatic presentation evaluation",
+      desc: "Generate a video processing application which will evaluate the presentation given by a student in categories by detecting presenter's confidence, expression, etc."
+    },
+    {
+      psno: "SBHRCCIIT008",
+      category: "Software",
+      theme: "Smart Education",
+      problemStat: "Automatic report evaluation",
+      desc: "Generate an image processing and NLP application to evaluate the report submitted by a student in categories based on some keywords from the given topic."
+    },
+    {
+      psno: "SBHRCCIIT009",
+      category: "Hardware",
+      theme: "Clean/Green Technology",
+      problemStat: "Detecting and monitoring the effect of pesticides in agriculture",
+      desc: "The system will detect and monitor the step by step effect of pesticides used in agriculture."
+    },
+    {
+      psno: "SBHRCCIIT010",
+      category: "Hardware",
+      theme: "Agriculture, Foodtech and Rural Development",
+      problemStat: "Create a smart farming system to increase agricultural yields and decrease waste",
+      desc: "The system will predict the best farming environment by optimizing the use of different parameters and reducing waste."
+    }
+  ]
+  
